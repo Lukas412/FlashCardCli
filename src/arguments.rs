d0a845bddd4dafc555a303b4d8092fb1b4b31bbc
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-30 22:14:15 +0200

    update arguments to use clap derive

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,11 +1,13 @@
 use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 pub(crate) struct FlashCardCli {
+    #[clap(subcommand)]
     command: FlashCardCommand,
 }
 
 #[derive(Debug, Subcommand)]
 enum FlashCardCommand {
-    Convert {},
+    Convert { card: PathBuf, csv: PathBuf },
 }
