d04b4c2039f80bb0000334d874d68b8dd82810d5
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-30 22:58:59 +0200

    add anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use crate::arguments::{FlashCardCli, FlashCardCommand};
 use clap::Parser;
+use std::fs;
 
 mod arguments;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let arguments = FlashCardCli::parse();
     match arguments.command() {
-        FlashCardCommand::Convert { card, csv } => {}
+        FlashCardCommand::Convert { card, csv } => {
+            let file = fs::read_to_string(card)?;
+        }
     }
+    Ok(())
 }
